cd6776ab986ae1040438d7f872272833aeb21c65
vivioliver <122638547+vivioliver@users.noreply.github.com>
2023-10-24 11:43:21 -0300

    poo

---
+++ poo/eletrodomestico.py
@@ -0,0 +1,30 @@
+class Eletrodomestico:
+    def __init__(self, ligado, voltagem, consumo):
+        self.ligado = ligado
+        self.voltagem = voltagem
+        self.consumo = consumo
+
+    #configurar valor
+    def set_valor_voltagem(self, novo_voltagem):
+        self.voltagem = novo_voltagem
+        #return voltagem
+
+    #retornar valor_voltagem
+    def get_valor_voltagem(self):
+        return self.voltagem
+
+    #configurar consumo
+    def set_valor_consumo(self, consumo):
+        self.consumo = consumo
+        #return consumo
+
+    #configurar valor_consumo
+    def get_valor_consumo(self):
+        return self.voltagem
+
+
+eletr = Eletrodomestico(False, 110, 150)
+eletr.set_valor_voltagem(520)
+print(eletr.get_valor_voltagem{valor_voltagem})
+eletr.set_valor_consumo(110)
+print(eletr.get_valor_consumo{valor_consumo})
